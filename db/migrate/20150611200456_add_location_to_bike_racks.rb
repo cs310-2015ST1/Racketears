f7e6d482588062e802cce524909bd5ac10eb5621
Paul Tran <ptranx23@gmail.com>
2015-06-16 07:25:09 -0700

    changed database from sqlite to postgresql

--- db/migrate/20150611200456_add_location_to_bike_racks.rb
+++ db/migrate/20150611200456_add_location_to_bike_racks.rb
@@ -1,6 +1,6 @@
 class AddLocationToBikeRacks < ActiveRecord::Migration
   def change
-    add_column :bike_racks, :latitude, :double
-    add_column :bike_racks, :longitude, :double
+    add_column :bike_racks, :latitude, :float
+    add_column :bike_racks, :longitude, :float
   end
 end
